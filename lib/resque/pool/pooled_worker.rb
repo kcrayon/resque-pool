e83c081bd50f02fe2c21206bb82cd17cd8d96487
kcrayon <github.com@crayon.at>
2012-04-17 00:44:07 -0700

    Really fix shutdown?

--- lib/resque/pool/pooled_worker.rb
+++ lib/resque/pool/pooled_worker.rb
@@ -2,18 +2,20 @@ require 'resque/worker'
 
 class Resque::Pool
   module PooledWorker
-
     # override +shutdown?+ method
     def shutdown_with_pool
       shutdown_without_pool || Process.ppid == 1
     end
 
+    def self.included(base)
+      base.instance_eval do
+        alias_method :shutdown_without_pool, :shutdown?
+        alias_method :shutdown?, :shutdown_with_pool
+      end
+    end
   end
 end
 
 Resque::Worker.class_eval do
   include Resque::Pool::PooledWorker
-
-  alias_method :shutdown_without_pool, :shutdown?
-  alias_method :shutdown?, :shutdown_with_pool
 end
